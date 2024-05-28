b5c539ae17957ab8d5c3d307c54547bcee2966ee
Amin Belalia <belaliab1u@etu.univ-lorraine.fr>
2024-05-28 16:01:12 +0200

    Ajout de arc et Arcs

--- src/Arcs.java
+++ src/Arcs.java
@@ -1,15 +1,14 @@
-import java.util.List;
-
+import java.util.*;
 public class Arcs {
     private List<Arc> arcs;
-    public Arcs(){
+
+    public Arcs() {
         this.arcs = new ArrayList<Arc>();
     }
+    public void ajouterArc(Arc a) {
+        this.arcs.add(a);
+    }
+    public List<Arc> getArcs() {
+        return this.arcs;
+    }
 }
-public void ajouterArc(Arc a){
-    this.arcs.add(a);
-}
-public List<Arc> getArcs(){
-    return this.arcs;
-}
-
